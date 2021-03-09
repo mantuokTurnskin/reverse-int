d848a5ec6e671c13db6d2fe6cc47ebfa8caa4e8d
Mantuok <etigiloff@gmail.com>
2021-03-09 21:57:44 +0300

    init: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let result = ''
+    for (let i = String(n).length - 1; i >= 0; i--) {
+        result += String(n)[i];
+    }
+    return parseInt(result);
 }
+
